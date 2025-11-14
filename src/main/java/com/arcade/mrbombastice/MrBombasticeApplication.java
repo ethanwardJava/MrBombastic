bd5ae69d89dea25ae358c06c4ab294122c30fe4d
alireza <theethanwardhere@gmail.com>
2025-11-14 11:31:08 +0330

    v1.0.0

---
+++ src/main/java/com/arcade/mrbombastice/MrBombasticeApplication.java
@@ -0,0 +1,13 @@
+package com.arcade.mrbombastice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrBombasticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MrBombasticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arcade/mrbombastice/MrBombasticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arcade.mrbombastice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrBombasticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
